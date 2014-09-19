5458cfe0de67cfe1d594ae5664962798b6b23efa
Chris Collins <collins.christopher@gmail.com>
2014-09-19 09:27:27 -0400

    prepping WP for auto-install

--- setup-wp/setup-wordpress.sh
+++ setup-wp/setup-wordpress.sh
@@ -1,11 +1,55 @@
-#!/bin/sh
+#!/bin/bash
 
-if [[ "$(ls -A /var/www/html)" ]] ; then
-  echo "SOMETHING ALREADY INSTALLED"
+if [[ -z "$APP_DB" ]] ; then
+  echo '$APP_DB environmental variable not set in container'
+  exit 1
+elif [[ -z "$APP_USER" ]] ; then
+  echo '$APP_USER environmental variable not set in container'
+  exit 1
+elif [[ -z "$APP_PASS" ]] ; then
+  echo '$APP_PASS environmental variable not set in container'
+  exit 1
+elif [[ -z "$DB_PORT_3306_TCP_ADDR" ]] ; then
+  echo '$DB_PORT_3306_TCP_ADDR environmental variable not set'
+  echo 'Is this container linked with the database container as "db"?'
+  exit 1
+fi
+
+DB_NAME=$APP_DB
+DB_USER=$APP_USER
+DB_PASS=$APP_PASS
+DB_HOST=$DB_PORT_3306_TCP_ADDR
+
+if [[ $1 == "--shell" ]] ; then
+  RUNSHELL='true'
+fi
+
+if [[ "$(/bin/ls -A /var/www/html)" ]] ; then
+  /bin/echo "SOMETHING ALREADY INSTALLED IN '/var/www/html'"
   exit 1
 else
   cd /
-  unzip /latest.zip
-  rsync -az /wordpress/ /var/www/html/
-  rm -rf /wordpress
+  /usr/bin/unzip /latest.zip
+  /usr/bin/rsync -az /wordpress/ /var/www/html/
+  /bin/rm -rf /wordpress
+
+  /bin/sed -e "s/database_name_here/$DB_NAME/
+  s/username_here/$DB_USER/
+  s/password_here/$DB_PASS/
+  s/localhost/$DB_HOST/
+  /'AUTH_KEY'/s/put your unique phrase here/$(pwgen -c -n -1 65)/
+  /'SECURE_AUTH_KEY'/s/put your unique phrase here/$(pwgen -c -n -1 65)/
+  /'LOGGED_IN_KEY'/s/put your unique phrase here/$(pwgen -c -n -1 65)/
+  /'NONCE_KEY'/s/put your unique phrase here/$(pwgen -c -n -1 65)/
+  /'AUTH_SALT'/s/put your unique phrase here/$(pwgen -c -n -1 65)/
+  /'SECURE_AUTH_SALT'/s/put your unique phrase here/$(pwgen -c -n -1 65)/
+  /'LOGGED_IN_SALT'/s/put your unique phrase here/$(pwgen -c -n -1 65)/
+  /'NONCE_SALT'/s/put your unique phrase here/$(pwgen -c -n -1 65)/" \
+  /var/www/html/wp-config-sample.php > /var/www/html/wp-config.php
+
+  /bin/chown -R apache /var/www/html
+fi
+
+if [[ $RUNSHELL == 'true' ]] ; then
+  exec /bin/bash
 fi
