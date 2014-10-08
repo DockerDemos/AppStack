1f067e9de0d687c4eae345017c7fafbeac757e90
Chris Collins <collins.christopher@gmail.com>
2014-10-08 15:22:34 -0400

    removed unused code

--- redis/run.sh
+++ redis/run.sh
@@ -3,10 +3,6 @@
 RUN="$1"
 HOST="$(env |awk -F= '/_PORT_6379_TCP_ADDR/ {print $2}')"
 
-if [[ ! -z $2 ]] ; then
-  CONTAINER_NAME="$(echo $2 |tr [:lower:] [:upper:])"
-fi
-
 case "$1" in
   redis-server)
     exec redis-server
